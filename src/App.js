ea931d289232f196965f151d0b9313334b208b13
sava <savamedvedev@gmail.com>
2022-04-22 18:38:47 +0800

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-	<h1>Food Menu</h1>
+    <>
+      <Router>
+        <Header />
+        <main className="container content">
+          <Switch>
+            <Route />
+          </Switch>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
